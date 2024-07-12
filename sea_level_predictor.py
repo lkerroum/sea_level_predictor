2097879dd3f1663f4b1fddbca468d7bd5656e3ad
Leila Kerroum <leila.kerroum@estudiantat.upc.edu>
2024-07-12 06:58:10 +0000

    first best fit

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,12 +4,18 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv("epa-sea-level.csv")
 
     # Create scatter plot
-
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], label='Scatter')
 
     # Create first line of best fit
+    lr = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x = df['Year']
+    y = lr.intercept + x*lr.slope
+    plt.plot(x,y, label='Best fit 1', color=orange)
+    plt.legend() 
+    plt.show()
 
 
     # Create second line of best fit
@@ -21,3 +27,5 @@ def draw_plot():
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
+
+draw_plot()
